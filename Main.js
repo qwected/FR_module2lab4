5a2762a6989ca6a693eb7ff2311eba013cea0d69
qwected <stepan.chibrikov@gmail.com>
2025-02-27 12:51:19 +0400

    Main.js

---
+++ Main.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Transport_1 = require("./Transport");
+var owner = new Transport_1.Person('Иванов', 'Иван', 'Иванович', new Date(1985, 6, 15), Transport_1.CustomDocumentType.Passport, '1234', '567890');
+var car = new Transport_1.Automobile('Toyota', 'Camry', 2022, 'JTDBE32K502109123', 'А123ВС77', owner, Transport_1.CarBodyType.Sedan, Transport_1.CarClass.Business);
+var bike = new Transport_1.Motorcycle('Yamaha', 'YZF-R1', 2021, 'JYARN23E5DA004321', 'Б456СД78', owner, 'Спортивная', true);
+var garage = new Transport_1.Garage(new Date());
+garage.addVehicle(car);
+garage.addVehicle(bike);
+console.log('--- Владелец ---');
+owner.displayInfo();
+console.log('--- Автомобиль ---');
+car.displayVehicleInfo();
+console.log('--- Мотоцикл ---');
+bike.displayVehicleInfo();
+console.log('--- Список ТС в гараже ---');
+var vehicles = garage.getAllVehicles();
+vehicles.forEach(function (vehicle) { return vehicle.displayVehicleInfo(); });
